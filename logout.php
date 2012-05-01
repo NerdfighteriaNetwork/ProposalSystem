176e6956da0376859b15e64d4e2b759c25aa33f3
Timothy Zorn <timothy.zorn@zornco.net>
2012-05-01 17:48:27 -0400

    type-o :P
    Signed-off-by: Timothy Zorn <timothy.zorn@zornco.net>

--- logout.php
+++ logout.php
@@ -3,10 +3,10 @@
 $_SESSION['loggedin'] = 0;
 if(isset($_SERVER['HTTP_REFERER']) && $_SERVER['HTTP_REFERER'])
 {
-	header( "Location: ".substr($_SERVER['HTTP_REFERER'],0,strrpost($_SERVER['HTTP_REFERER'],"/")) );
+	header( "Location: ".substr($_SERVER['HTTP_REFERER'],0,strrpos($_SERVER['HTTP_REFERER'],"/")) );
 }
 else
 {
-	header( "Location: ".substr($_SERVER['REQUEST_URI'],0,strpost($_SERVER['REQUEST_URI'],"/")) );
+	header( "Location: ".substr($_SERVER['REQUEST_URI'],0,strpos($_SERVER['REQUEST_URI'],"/")) );
 }
 ?>
