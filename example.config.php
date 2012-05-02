7da74f6229b4e3ea80e167bdbb717792d25e875e
Tyrope <Tyrope@TyRope.nl>
2012-05-02 20:10:40 +0200

    Adding password encryption configuration options.
    Signed-off-by: Tyrope <Tyrope@TyRope.nl>

--- example.config.php
+++ example.config.php
@@ -10,6 +10,10 @@ $conf['sql']['server'] = "localhost";
 $conf['sql']['database'] = "proposalSystem";
 $conf['sql']['table_prefix'] = "PS_";
 
+//password encryption settings.
+$conf['salt']['useFile'] = FALSE; //If this is TRUE, passwords will use salt.php to generate a salt. This file is not open-source for security reasons.
+$conf['salt']['default'] = 'AnPtb6!Dy5g$QGTg#3dUFy)39Crq{sR2pG2sY@P7FTb&5RaF'; //if salt useFile is FALSE, this string is used as the salt. We recommend you generate your own random string for security purposes.
+
 // Comment out the next line by putting // in front of it!
 die("Hey... Read the config file!");
 ?>
